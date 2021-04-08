dfc5ce9d323a4ee7b332b4a443d85e6b1f49f18a
Brian Baldner <42284551+brianbaldner@users.noreply.github.com>
2021-04-07 22:34:45 -0700

    commit

--- ValNugetDemo/Program.cs
+++ ValNugetDemo/Program.cs
@@ -6,7 +6,7 @@ namespace ValAPINetDemo
     {
         static void Main(string[] args)
         {
-            Auth au = Auth.Login("bigtaco21", "Epicbrian05", Region.NA);
+            Auth au = Auth.Login("username", "password", Region.NA);
             Storefront mmrvas = Storefront.GetOffers(au);
             Console.WriteLine(mmrvas.SkinsPanelLayout.SingleItemOffers[0]);
             Console.ReadKey();
