8951f103c446ce2d475e01d230419b6ac5ca2318
Brian Baldner <42284551+brianbaldner@users.noreply.github.com>
2021-04-16 07:36:03 -0700

    Update username.cs

--- ValNuget/username.cs
+++ ValNuget/username.cs
@@ -39,5 +39,32 @@ namespace ValAPINet
             //ret.responce = responce;
             return ret;
         }
+
+public static List<Username> GetUsername(Auth au, List<string> playerids)
+        {
+            Username ret = new Username();
+            if (playerid == "useauth")
+            {
+                playerid = au.subject;
+            }
+            string url = "https://pd." + au.region + ".a.pvp.net/name-service/v2/players";
+            RestClient client = new RestClient(url);
+            client.CookieContainer = au.cookies;
+            //client.CookieContainer = new CookieContainer();
+
+            RestRequest request = new RestRequest(Method.PUT);
+            request.AddHeader("Authorization", $"Bearer {au.AccessToken}");
+            request.AddHeader("X-Riot-Entitlements-JWT", $"{au.EntitlementToken}");
+            request.AddHeader("X-Riot-ClientPlatform", $"ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9");
+            request.AddHeader("X-Riot-ClientVersion", $"{au.version}");
+            request.AddJsonBody(playerids);
+            var responce = client.Execute(request);
+            string responcecontent = responce.Content;
+            List<Username> list = JsonConvert.DeserializeObject<List<Username>>(responcecontent);
+            list[0].StatusCode = (int)responce.StatusCode;
+            //ret.responce = responce;
+            return list;
+        }
+
     }
 }
